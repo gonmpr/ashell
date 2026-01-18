d9fc64c12b9f7ce36afe5eb939aa3c2627585b68
gonmpr <gmporrorosas@gmail.com>
2026-01-17 23:38:12 -0300

    setting up

---
+++ run.sh
@@ -0,0 +1,2 @@
+make src/main
+src/main
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  setbuf(stdout, NULL);
+
+  printf("$ ");
+
+  return 0;
+}
